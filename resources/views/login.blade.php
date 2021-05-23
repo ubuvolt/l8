b750db2738ffdf4df395377868a549c55ef205f0
Ubu Volt <ubuvolt@gmail.com>
2021-05-23 17:17:08 +0100

    dashboard view

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,12 +1,11 @@
-@extends('layouts.app')
+@extends('layouts.login')
 
 @section('content')
 <!-- Outer Row -->
 <div class="row justify-content-center">
-
     <div class="col-xl-10 col-lg-12 col-md-9">
-
         <div class="card o-hidden border-0 shadow-lg my-5">
+
             <div class="card-body p-0">
                 <!-- Nested Row within Card Body -->
                 <div class="row">
@@ -16,15 +15,17 @@
                             <div class="text-center">
                                 <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                             </div>
-                            <form class="user">
+
+                            <form class="user" method="POST" action="{{ route('login') }}">
+                                {{ csrf_field() }}
                                 <div class="form-group">
                                     <input type="email" class="form-control form-control-user"
-                                           id="exampleInputEmail" aria-describedby="emailHelp"
+                                           name="inputEmail" aria-describedby="emailHelp"
                                            placeholder="Enter Email Address...">
                                 </div>
                                 <div class="form-group">
                                     <input type="password" class="form-control form-control-user"
-                                           id="exampleInputPassword" placeholder="Password">
+                                           name="inputPassword" placeholder="Password">
                                 </div>
                                 <div class="form-group">
                                     <div class="custom-control custom-checkbox small">
@@ -33,31 +34,23 @@
                                             Me</label>
                                     </div>
                                 </div>
-                                <a href="index.html" class="btn btn-primary btn-user btn-block">
+
+                                @if(Session::has('error'))
+                                <p class="alert alert-danger">{{ Session::get('error') }}</p>
+                                @endif
+
+                                <button type="submit" class="btn btn-primary btn-user btn-block">
                                     Login
-                                </a>
-                                <hr>
-                                <a href="index.html" class="btn btn-google btn-user btn-block">
-                                    <i class="fab fa-google fa-fw"></i> Login with Google
-                                </a>
-                                <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                    <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                </a>
+                                </button>
+
                             </form>
-                            <hr>
-                            <div class="text-center">
-                                <a class="small" href="forgot-password.html">Forgot Password?</a>
-                            </div>
-                            <div class="text-center">
-                                <a class="small" href="register.html">Create an Account!</a>
-                            </div>
+
                         </div>
                     </div>
                 </div>
             </div>
+
         </div>
-
     </div>
-
 </div>
 @endsection
