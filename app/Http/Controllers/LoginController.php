f4c0b714f7878dc91888a0d820f189253764204a
Ubu Volt <ubuvolt@gmail.com>
2021-05-23 17:16:04 +0100

    dashboard controllers

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller {
+
+    public function login(Request $request) {
+        $inputEmail = $request->input('inputEmail');
+        $inputPassword = $request->input('inputPassword');
+
+        if ($this->loginDetails($inputEmail, $inputPassword)) {
+            return redirect('dashboard');
+        } else {
+            return redirect()->back()->with('error', 'Login Failed!');
+        }
+    }
+
+    /**
+     * @param string $inputEmail
+     * @param string $inputPassword
+     * 
+     * @return boolean
+     */
+    private function loginDetails(string $inputEmail, string $inputPassword): bool {
+
+        $loginValidation = false;
+        $loginDetailsArray = ['ubuvolt@gmail.com' => 'Hertiet#42'];
+
+        if (array_key_exists($inputEmail, $loginDetailsArray)) {
+            if ($loginDetailsArray[$inputEmail] == $inputPassword) {
+                $loginValidation = true;
+            }
+        }
+
+        return $loginValidation;
+    }
+
+}
