3da02a38559ce10aa6c0a0f8459fa40f1e0bc55a
Ubu Volt <ubuvolt@gmail.com>
2021-05-23 17:17:38 +0100

    dashboard web

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
 
 Route::get('/', function () {
     return view('login');
 });
+
+Route::match(array('GET', 'POST'), 'login', 'App\Http\Controllers\LoginController@login')->name('login');
+Route::match(array('GET', 'POST'), 'dashboard', 'App\Http\Controllers\DashboardController@dashboard')->name('dashboard');
